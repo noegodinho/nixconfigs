5a71c0964b05995ffb71178bbd98746ee0784d61
noegodinho <noe.godinho.92@gmail.com>
2024-06-15 19:55:21 +0100

    Trying home manager config.

--- configfiles/albireo/albireo.nix
+++ configfiles/albireo/albireo.nix
@@ -145,6 +145,8 @@
     ];
   };
 
+  programs.zsh.enable = true;
+
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
