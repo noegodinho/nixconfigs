1496db605e002d232f503f23c3c66a0dbb1a43b4
noegodinho <noe.godinho.92@gmail.com>
2024-06-16 01:05:18 +0100

    Configure oh-my-zsh in home manager. TODO: program plugins into nix instead of downloading from github manually.

--- configfiles/albireo/home.nix
+++ configfiles/albireo/home.nix
@@ -93,8 +93,6 @@
         };
 
         initExtra=''
-          eval "$(zellij setup --generate-auto-start zsh)"
-          eval "$(atuin init zsh)"
           source ~/powerlevel10k/powerlevel10k.zsh-theme
           source ~/.p10k.zsh
           source ~/zsh-autocomplete/zsh-autocomplete.plugin.zsh
@@ -116,22 +114,6 @@
       java.enable = true;
       home-manager.enable = true;
   };
-
-  # programs.bash = {
-  #   enable = true;
-  #   enableCompletion = true;
-  #   # TODO add your custom bashrc here
-  #   bashrcExtra = ''
-  #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-  #   '';
-  # 
-  #   # set some aliases, feel free to add more or remove some
-  #   shellAliases = {
-  #     k = "kubectl";
-  #     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-  #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-  #   };
-  # };
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
