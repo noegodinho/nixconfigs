5d2ea918f70816bfd5627db41e21003b9ef39b93
noegodinho <noe.godinho.92@gmail.com>
2024-06-16 18:53:24 +0100

    Updated flakes.

--- configfiles/albireo/flake.nix
+++ configfiles/albireo/flake.nix
@@ -1,10 +1,12 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    
     nix-flatpak = {
       url = "github:gmodena/nix-flatpak"; # unstable branch. Use github:gmodena/nix-flatpak/?ref=<tag> to pin releases.
     };
