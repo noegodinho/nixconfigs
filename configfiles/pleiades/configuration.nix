5a8e1412975c1d5cc531c81ca796f5a53fd68d56
noegodinho <noe.godinho.92@gmail.com>
2024-08-30 21:35:38 +0100

    Added extra bluetooth settings.

--- configfiles/pleiades/configuration.nix
+++ configfiles/pleiades/configuration.nix
@@ -219,12 +219,12 @@
   hardware.bluetooth.enable = true; # enables support for Bluetooth
   hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
 
-  # hardware.bluetooth.settings = {
-  #   General = {
-  #     Enable = "Source,Sink,Media,Socket";
-  #     Experimental = true;
-  #   };
-  # };
+  hardware.bluetooth.settings = {
+    General = {
+      Enable = "Source,Sink,Media,Socket";
+      Experimental = true;
+    };
+  };
 
   # hardware.pulseaudio = {
   #  enable = true;
