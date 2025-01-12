a641ab5c0838997beba0aff598cb92daff891df0
noegodinho <noe.godinho@protonmail.com>
2025-01-12 00:06:22 +0000

    Changed and removed packages and configurations.

--- configfiles/albireo/configuration.nix
+++ configfiles/albireo/configuration.nix
@@ -138,6 +138,8 @@
 
   services.xserver.excludePackages = [ pkgs.xterm ];
 
+  services.fwupd.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
