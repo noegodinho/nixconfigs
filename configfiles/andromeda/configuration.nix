b3a29c7d01e438746bc148d49a17f1dbeeef256a
noegodinho <noe.godinho@protonmail.com>
2025-01-13 22:50:24 +0000

    Reorganising configuration files.

--- configfiles/andromeda/configuration.nix
+++ configfiles/andromeda/configuration.nix
@@ -6,6 +6,7 @@
 {
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ./services-configuration.nix
     ./vpn.nix
   ];
 
@@ -21,22 +22,22 @@
     };
   };
 
-  networking.hostName = "laniakea"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking = {
+    # Define your hostname.
+    hostName = "laniakea";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Configure network proxy if necessary
+    # proxy.default = "http://user:password@proxy:port/";
+    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager = {
-    enable = true;
-    wifi.powersave = false;
+    # wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+    # Enable networking
+    networkmanager = {
+      enable = true;
+      wifi.powersave = false;
+    };
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.libinput.enable = true;
-  services.libinput.touchpad.naturalScrolling = true;
 
   # Set your time zone.
   time.timeZone = "Europe/Lisbon";
@@ -57,47 +58,76 @@
     };
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  # Configure keymap in X11
-  services.xserver = {
-    enable = true;
-    xkb = {
-      layout = "pt";
-      variant = "";
-    };
-  };
-
   # Configure console keymap
   console.keyMap = "pt-latin1";
 
-  # Enable the KDE Plasma Desktop Environment.
-  services = {
-    desktopManager.plasma6.enable = true;
-    displayManager.sddm = {
+  hardware = { 
+    # Update the Intel microcode on boot.
+    cpu.intel.updateMicrocode = true;
+
+    # Graphic drivers
+    graphics = {
       enable = true;
-      wayland.enable = true;
+      enable32Bit = true;
+
+      extraPackages = with pkgs; [
+        intel-media-driver # LIBVA_DRIVER_NAME=iHD
+        intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+        libvdpau-va-gl
+        intel-compute-runtime
+        vpl-gpu-rt
+      ];
+    };
+
+    # enables support for Bluetooth & powers up the default Bluetooth controller on boot
+    bluetooth = {
+      enable = true;
+      package = nixpkgs-unstable.legacyPackages."${pkgs.system}".bluez;
+      powerOnBoot = true;
+      settings = {
+        General = {
+          Enable = "Source,Sink,Media,Socket";
+          Experimental = true;
+          JustWorksRepairing = "always";
+        };
+      };
+    };
+
+    # Enable sound with pipewire.
+    pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+
+    # Enable logitech wireless mouse
+    logitech.wireless.enable = true;
+
+    # printers = {
+    #   ensurePrinters = [
+    #     {
+    #       name = "printer-hall-2";
+    #       location = "DEI Main Hall";
+    #       deviceUri = "http://ipp.dei.uc.pt/printers/printer-hall-2";
+    #       model = "drv:///home/andromeda/printer_drivers/KMbeu750iux.ppd";
+    #       ppdOptions = {
+    #         PageSize = "A4";
+    #       };
+    #     }
+    #   ];
+    #   ensureDefaultPrinter = "printer-hall-2";
+    # };
+  };
+
+  nix = {
+    # Nix settings
+    settings.experimental-features = [ 
+      "nix-command" "flakes" 
+    ];
+
+    # Nix garbage collector
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
   };
-
-  # Update the Intel microcode on boot.
-  hardware.cpu.intel.updateMicrocode = true;
-
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-
-    extraPackages = with pkgs; [
-      intel-media-driver # LIBVA_DRIVER_NAME=iHD
-      intel-vaapi-driver # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
-      libvdpau-va-gl
-      intel-compute-runtime
-      vpl-gpu-rt
-    ];
-  };
-
-  # Nix settings
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nixpkgs.config = {
     # allow proprietary packages
@@ -114,13 +144,6 @@
     # };
   };
 
-  # Nix garbage collector
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
   # Autoupgrade system
   system.autoUpgrade = {
     enable = true;
@@ -128,87 +151,26 @@
     allowReboot = false; # decide later
   };
 
-  # Enable CUPS to print documents.
-  services.printing = {
-    enable = true;
-    cups-pdf.enable = true;
-    logLevel = "debug2";
+  security = {
+    # Enable real-time sound
+    rtkit.enable = true;
 
-    # drivers = [
-    #   (pkgs.writeTextDir "share/cups/model/KMbeu750iux.ppd" (builtins.readFile /home/andromeda/printer_drivers/KMbeu750iux.ppd))
-    # ];
-
-    # extraConf = ''
-    #   Browsing Yes
-    #   DefaultShared Yes
-    #   DefaultAuthType Basic
-    #   WebInterface Yes
-    # '';
-    # extraFilesConf = ''
-    #   RemoteRoot noe
-    # '';
+    # Disable OS limits
+    pam.loginLimits = [
+      {
+        domain = "*";
+        type = "-";
+        item = "memlock";
+        value = "unlimited";
+      }
+      {
+        domain = "*";
+        type = "-";
+        item = "rtprio";
+        value = "99";
+      }
+    ];
   };
-
-  services.avahi = {
-    enable = true;
-    nssmdns4 = true;
-    openFirewall = true;
-  };
-
-  # hardware.printers = {
-  #   ensurePrinters = [
-  #     {
-  #       name = "printer-hall-2";
-  #       location = "DEI Main Hall";
-  #       deviceUri = "http://ipp.dei.uc.pt/printers/printer-hall-2";
-  #       model = "drv:///home/andromeda/printer_drivers/KMbeu750iux.ppd";
-  #       ppdOptions = {
-  #         PageSize = "A4";
-  #       };
-  #     }
-  #   ];
-  #   ensureDefaultPrinter = "printer-hall-2";
-  # };
-
-  # enables support for Bluetooth & powers up the default Bluetooth controller on boot
-  hardware.bluetooth = {
-    enable = true;
-    package = nixpkgs-unstable.legacyPackages."${pkgs.system}".bluez;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-        Experimental = true;
-        JustWorksRepairing = "always";
-      };
-    };
-  };
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  security.pam.loginLimits = [
-    {
-      domain = "*";
-      type = "-";
-      item = "memlock";
-      value = "unlimited";
-    }
-    {
-      domain = "*";
-      type = "-";
-      item = "rtprio";
-      value = "99";
-    }
-  ];
 
   # Man docs
   documentation = {
@@ -220,10 +182,6 @@
       generateCaches = true;
     };
   };
-
-  # Enable automatic login for the user.
-  services.displayManager.autoLogin.enable = true;
-  services.displayManager.autoLogin.user = "andromeda";
 
   users.users.andromeda = {
     shell = pkgs.zsh;
@@ -268,32 +226,6 @@
   # BPF-based Linux IO analysis, networking, monitoring, and more
   programs.bcc.enable = true;
 
-  # fingerprint drivers
-  services.fprintd = {
-    enable = true;
-    package = pkgs.fprintd-tod;
-    tod = {
-      enable = true;
-      driver = pkgs.libfprint-2-tod1-vfs0090;
-    };
-  };
-
-  # services.thinkfan = {
-  #   enable = true;
-    # levels = [
-    #   [0    0      4]
-    #   [1    35     4]
-    #   [2    38     4]
-    #   [3    42     5]
-    #   [4    45     5]
-    #   [5    48     6]
-    #   [6    51     6]
-    #   [7    54     6]
-    #   [8    56     7]
-    #   [9    58    99]
-    # ];
-  # };
-
   nixpkgs.overlays = [
     (final: prev: {
       pcscliteWithPolkit = prev.pcscliteWithPolkit.overrideAttrs(oldAttrs: {
@@ -325,41 +257,7 @@
   environment.plasma6.excludePackages = with pkgs; [
     khelpcenter
     kdePackages.kate
-  ];
-
-  services.xserver.excludePackages = [
-    pkgs.xterm
-  ];
-
-  # "Driver" for MX Master 3S
-  hardware.logitech.wireless.enable = true;
-  services.solaar = {
-    enable = true;
-    package = nixpkgs-unstable.legacyPackages."${pkgs.system}".solaar;
-    window = "hide"; # Show the window on startup (show, *hide*, only [window only])
-    batteryIcons = "regular"; # Which battery icons to use (*regular*, symbolic, solaar)
-    extraArgs = ""; # Extra arguments to pass to solaar on startup
-  };
-
-  # Enable flatpak for sandboxed applications
-  services.flatpak.enable = true;
-
-  systemd.services.flatpak-repo = {
-    wantedBy = [ "multi-user.target" ];
-    path = [ pkgs.flatpak ];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
-  };
-
-  # Smart card reader driver
-  services.pcscd.enable = true;
-
-  # Firmware and BIOS updates
-  services.fwupd = {
-    enable = true;
-    package = nixpkgs-unstable.legacyPackages."${pkgs.system}".fwupd;
-  };
+  ];  
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
---
+++ configfiles/andromeda/services-configuration.nix
@@ -0,0 +1,129 @@
+{ pkgs, nixpkgs-unstable, ... }:
+{
+  services = {
+    # Enable touchpad support (enabled default in most desktopManager).
+    # ibinput.enable = true;
+    libinput.touchpad.naturalScrolling = true;
+
+    # Enable the X11 windowing system.
+    # You can disable this if you're only using the Wayland session.
+    # Configure keymap in X11
+    xserver = {
+      enable = true;
+      xkb = {
+        layout = "pt";
+        variant = "";
+      };
+      # Packages to exclude from KDE
+      excludePackages = with pkgs; [
+        xterm
+      ];
+    };
+
+    # Enable the KDE Plasma Desktop Environment.
+    desktopManager.plasma6.enable = true;
+    displayManager = {
+      sddm = {
+        enable = true;
+        wayland.enable = true;
+      };
+      # Enable automatic login for the user.
+      autoLogin = {
+        enable = true;
+        user = "andromeda";
+      };
+    };
+
+    # Enable CUPS to print documents.
+    printing = {
+      enable = true;
+      cups-pdf.enable = true;
+      logLevel = "debug2";
+
+      # drivers = [
+      #   (pkgs.writeTextDir "share/cups/model/KMbeu750iux.ppd" (builtins.readFile /home/andromeda/printer_drivers/KMbeu750iux.ppd))
+      # ];
+
+      # extraConf = ''
+      #   Browsing Yes
+      #   DefaultShared Yes
+      #   DefaultAuthType Basic
+      #   WebInterface Yes
+      # '';
+      # extraFilesConf = ''
+      #   RemoteRoot noe
+      # '';
+    };
+
+    avahi = {
+      enable = true;
+      nssmdns4 = true;
+      openFirewall = true;
+    };
+
+    # Sound service
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    # Fingerprint drivers
+    fprintd = {
+      enable = true;
+      package = pkgs.fprintd-tod;
+      tod = {
+        enable = true;
+        driver = pkgs.libfprint-2-tod1-vfs0090;
+      };
+    };
+
+    # "Driver" for MX Master 3S
+    solaar = {
+      enable = true;
+      package = nixpkgs-unstable.legacyPackages."${pkgs.system}".solaar;
+      window = "hide"; # Show the window on startup (show, *hide*, only [window only])
+      batteryIcons = "regular"; # Which battery icons to use (*regular*, symbolic, solaar)
+      extraArgs = ""; # Extra arguments to pass to solaar on startup
+    };
+
+    # Enable flatpak for sandboxed applications
+    flatpak.enable = true;
+
+    # Smart card reader driver
+    pcscd.enable = true;
+
+    # Firmware and BIOS updates
+    fwupd = {
+      enable = true;
+      package = nixpkgs-unstable.legacyPackages."${pkgs.system}".fwupd;
+    };
+
+    # Detailed fan control
+    # thinkfan = {
+      # enable = true;
+      # levels = [
+      #   [0    0      4]
+      #   [1    35     4]
+      #   [2    38     4]
+      #   [3    42     5]
+      #   [4    45     5]
+      #   [5    48     6]
+      #   [6    51     6]
+      #   [7    54     6]
+      #   [8    56     7]
+      #   [9    58    99]
+      # ];
+    # };
+  };
+
+  systemd.services.flatpak-repo = {
+    wantedBy = [ "multi-user.target" ];
+    path = [ pkgs.flatpak ];
+    script = ''
+      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+    '';
+  };
+}
