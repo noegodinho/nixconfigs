082228530771246a967279f791c437e363b2c08f
noegodinho <noe.godinho@protonmail.com>
2025-12-08 00:37:30 +0000

    Updated from pkgs.system to pkgs.stdenv.hostPlatform.system and reorganized settings.

--- configfiles/andromeda/configuration.nix
+++ configfiles/andromeda/configuration.nix
@@ -119,7 +119,7 @@
     # Enables support for Bluetooth & powers up the default Bluetooth controller on boot
     bluetooth = {
       enable = true;
-      package = nixpkgs-unstable.legacyPackages."${pkgs.system}".bluez;
+      package = nixpkgs-unstable.legacyPackages."${pkgs.stdenv.hostPlatform.system}".bluez;
       powerOnBoot = true;
       settings = {
         General = {
@@ -262,18 +262,20 @@
     ];
   };
 
-  # Autoupgrade system
-  system.autoUpgrade = {
-    enable = true;
-    dates = "daily";
-    allowReboot = false;
-  };
+  system = {
+    # Autoupgrade system
+    autoUpgrade = {
+      enable = true;
+      dates = "daily";
+      allowReboot = false;
+    };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    stateVersion = "24.11"; # Did you read the comment?
+  };
 }
