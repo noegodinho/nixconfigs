37ae865cca1670f2e58388bd8a044e6270af0b15
noegodinho <noe.godinho@protonmail.com>
2026-02-17 22:49:37 +0000

    Updated commit rev for nix-vscode-extensions.

--- configfiles/andromeda/home.nix
+++ configfiles/andromeda/home.nix
@@ -3,7 +3,7 @@
     (import (builtins.fetchGit {
       url = "https://github.com/nix-community/nix-vscode-extensions";
       ref = "refs/heads/master";
-      rev = "789fa907861bb589b5f02a415704053787a63a03";
+      rev = "b82f1bb23901ac24bf973a4a3cbddf5857245c09";
     })).extensions.${system};
 in {
   # link the configuration file in current directory to the specified location in home directory
