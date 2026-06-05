1d08d5459defc624fc8b64c2909b17d6d795f294
noegodinho <noe.godinho@protonmail.com>
2026-06-05 19:41:47 +0100

    Added nix settings to improve speed of hyprland updates.

--- configfiles/andromeda/configuration.nix
+++ configfiles/andromeda/configuration.nix
@@ -216,9 +216,21 @@
 
   nix = {
     # Nix settings
-    settings.experimental-features = [ 
-      "nix-command" "flakes" 
-    ];
+    settings = {
+      substituters = [
+        "https://hyprland.cachix.org"
+      ];
+      
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ]
+      ;
+      experimental-features = [ 
+        "nix-command" "flakes" 
+      ];
+    };
+    
+    
 
     # Nix garbage collector
     gc = {
